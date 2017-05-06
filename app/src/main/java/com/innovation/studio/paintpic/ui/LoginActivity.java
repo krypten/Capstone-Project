5a98ed47f18dcd407c8b16439dfb485676181d80
krypten <chaitiagrawal@gmail.com>
2017-05-07 01:29:50 +0530

    feat: Added login screen view in app with fb login integration
    Added support to authenticate people with their Facebook credentials.
    Also added view for login screen showing sign-in options from Facebook and Google. Also added option to skip sign-in in the view. All the buttons are mocks only currently.

---
+++ app/src/main/java/com/innovation/studio/paintpic/ui/LoginActivity.java
@@ -0,0 +1,60 @@
+package com.innovation.studio.paintpic.ui;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.innovation.studio.paintpic.R;
+
+/**
+ * A login screen that offers login via email/password.
+ */
+public class LoginActivity extends AppCompatActivity {
+	CallbackManager mCallbackManager;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		mCallbackManager = CallbackManager.Factory.create();
+		final LoginManager loginManager = LoginManager.getInstance();
+		loginManager.registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+			@Override
+			public void onSuccess(final LoginResult loginResult) {
+				Toast.makeText(getApplicationContext(), "onSuccess", Toast.LENGTH_LONG);
+				MainActivity.sIsLoggedIn = true;
+			}
+
+			@Override
+			public void onCancel() {
+				Toast.makeText(getApplicationContext(), "onCancel", Toast.LENGTH_LONG);
+			}
+
+			@Override
+			public void onError(final FacebookException error) {
+				Toast.makeText(getApplicationContext(), "onError", Toast.LENGTH_LONG);
+			}
+		});
+		findViewById(R.id.login_button_skip).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				MainActivity.sIsLoggedIn = true;
+				finish();
+			}
+		});
+	}
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, final Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		mCallbackManager.onActivityResult(requestCode, resultCode, data);
+	}
+}
+
--- app/src/main/java/com/innovation/studio/paintpic/ui/MainActivity.java
+++ app/src/main/java/com/innovation/studio/paintpic/ui/MainActivity.java
@@ -1,18 +1,29 @@
 package com.innovation.studio.paintpic.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
 import com.innovation.studio.paintpic.R;
 
 /**
  * Main Activity displaying the content to the users.
  */
 public class MainActivity extends AppCompatActivity {
+	public static boolean sIsLoggedIn = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		FacebookSdk.sdkInitialize(getApplicationContext());
+		AppEventsLogger.activateApp(this);
+		if (!sIsLoggedIn) {
+			startActivity(new Intent(this, LoginActivity.class));
+		}
+		Toast.makeText(this, "hi", Toast.LENGTH_LONG);
 	}
 }
